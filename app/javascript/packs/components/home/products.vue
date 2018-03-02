26327d80232df20c2d9017cf496add668a5fc10a
Ivan <ivan.kirnev@gmail.com>
2018-03-02 18:44:17 +0200

    products

--- app/javascript/packs/components/home/products.vue
+++ app/javascript/packs/components/home/products.vue
@@ -3,11 +3,28 @@
     <nav-top></nav-top>
     <main id="main" role="main">
       <div class="container">
-        <div class='row'>
+        <div class="row">
           <nav-left></nav-left>
-          <div class='col-md-8'>
-            <h2>{{ $t('home.title') }}</h2>
-            <p>{{ $t('home.content') }}</p>
+          <div class="col">
+            <div class="catalog-block__title">Продукция
+              <p></p>
+            </div>
+            <div class="catalog-block__list">
+               <a class="catalog-block__list-content-item" href="#">
+                <div class="image"><img src="/assets/door-one.png"></div>
+                <p>Двери</p><span>от 7600 руб.</span></a><a class="catalog-block__list-content-item" href="#">
+                <div class="image"><img src="/assets/door-one.png"></div>
+                <p>Решетки</p><span>от 7600 руб.</span></a><a class="catalog-block__list-content-item" href="#">
+                <div class="image"><img src="/assets/door-one.png"></div>
+                <p>Ворота</p><span>от 7600 руб.</span></a><a class="catalog-block__list-content-item" href="#">
+                <div class="image"><img src="/assets/door-one.png"></div>
+                <p>Мангалы</p><span>от 7600 руб.</span></a>
+               <a class="catalog-block__list-content-item" href="#">
+                <div class="image"><img src="/assets/door-one.png"></div>
+                <p>Козырьки</p><span>от 7600 руб.</span></a><a class="catalog-block__list-content-item" href="#">
+                <div class="image"><img src="/assets/img_storage/stairs/screw/IMG_4436.jpeg"></div>
+                <p>Лестницы</p><span>от 7600 руб.</span></a>
+            </div>
           </div>
         </div>
       </div>
