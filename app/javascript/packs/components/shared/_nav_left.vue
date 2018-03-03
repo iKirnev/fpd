52e2f4583d4967003f3b7405f468b0dac135d8f6
Ivan <ivan.kirnev@gmail.com>
2018-03-03 18:59:44 +0200

    left nav

--- app/javascript/packs/components/shared/_nav_left.vue
+++ app/javascript/packs/components/shared/_nav_left.vue
@@ -2,16 +2,19 @@
   <b-navbar class="navbar-expand-md">
     <b-collapse is-nav id="nav_collapse">
       <b-nav vertical class="sidebar-nav">
-        	<b-nav-text><h2>{{ $t('nav_left.catalog') }}</h2></b-nav-text>
+        	<b-nav-text><span class="flag"></span><h2>{{ $t('nav_left.catalog') }}</h2></b-nav-text>
+          <b-dropdown-divider></b-dropdown-divider>
           <b-nav-text><h3>{{ $t('nav_left.by_stuff') }}</h3></b-nav-text>
           <template v-for="stuff in stuffs">
             <b-nav-item :to="{ name: 'doors_stuff_path', params: { slug: `${stuff.id}-${stuff.name}` } }">{{ $t(`nav_left.stuff.${stuff.name}`) }}</b-nav-item>
           </template>
+          <b-dropdown-divider></b-dropdown-divider>
           <b-nav-text><h3>{{ $t('nav_left.by_use') }}</h3></b-nav-text>
           <template v-for="use in uses">
             <b-nav-item :to="{ name: 'doors_use_path', params: { slug: `${use.id}-${use.name}` } }">{{ $t(`nav_left.use.${use.name}`) }}</b-nav-item>
           </template>
-          <b-nav-text><h2>{{ $t('nav_left.services') }}</h2></b-nav-text>
+          <b-dropdown-divider></b-dropdown-divider>
+          <b-nav-text><h3>{{ $t('nav_left.services') }}</h3></b-nav-text>
           <b-nav-item :to="{ name: 'doors_calculator_path' }">{{ $t('nav_left.doors_calculator') }}</b-nav-item>
       </b-nav>
     </b-collapse>
