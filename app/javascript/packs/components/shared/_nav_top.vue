b0db46116f57a760f5236f2c7362983de858d5be
Ivan <ivan.kirnev@gmail.com>
2018-03-07 13:22:34 +0200

    investigating ios issue

--- app/javascript/packs/components/shared/_nav_top.vue
+++ app/javascript/packs/components/shared/_nav_top.vue
@@ -19,7 +19,7 @@
         <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
         <b-collapse is-nav id="nav_collapse">
           <b-navbar-nav>
-            <b-nav-item-dropdown :class="activeOn(['about_us_path','about_certificate_path','about_references_path','about_feedbacks_path'])">
+            <b-nav-item-dropdown>
               <template slot="button-content">
                 {{ $t('nav.about.company') }}
               </template>
@@ -28,11 +28,11 @@
               <b-dropdown-item :to="{ name: 'about_references_path' }">{{ $t('nav.about.references') }}</b-dropdown-item>
               <b-dropdown-item :to="{ name: 'about_feedbacks_path' }">{{ $t('nav.about.feedbacks') }}</b-dropdown-item>
             </b-nav-item-dropdown>
-            <b-nav-item :to="{ name: 'products_path' }" :class="activeOn(['products_path', 'doors_path', 'doors_result_path'])">{{ $t('nav.products') }}</b-nav-item>
-            <b-nav-item :to="{ name: 'galleries_path' }" :class="activeOn(['galleries_path'])">{{ $t('nav.galleries') }}</b-nav-item>
-            <b-nav-item :to="{ name: 'furniture_path' }" :class="activeOn(['furniture_path'])">{{ $t('nav.furniture') }}</b-nav-item>
-            <b-nav-item :to="{ name: 'price_path' }" :class="activeOn(['price_path'])">{{ $t('nav.price') }}</b-nav-item>
-            <b-nav-item :to="{ name: 'contacts_path' }" :class="activeOn(['contacts_path'])">{{ $t('nav.contacts') }}</b-nav-item>
+            <b-nav-item :to="{ name: 'products_path' }">{{ $t('nav.products') }}</b-nav-item>
+            <b-nav-item :to="{ name: 'galleries_path' }">{{ $t('nav.galleries') }}</b-nav-item>
+            <b-nav-item :to="{ name: 'furniture_path' }">{{ $t('nav.furniture') }}</b-nav-item>
+            <b-nav-item :to="{ name: 'price_path' }">{{ $t('nav.price') }}</b-nav-item>
+            <b-nav-item :to="{ name: 'contacts_path' }">{{ $t('nav.contacts') }}</b-nav-item>
             
           </b-navbar-nav>
         </b-collapse>
@@ -41,28 +41,4 @@
     <div xclass="send-request"><a href="#"></a></div>
   </header>
 </template>
-
-<script>
-export default {
-
-  data: function() {
-    return {
-      locale: window.I18n.locale,
-      availableLocales: window.I18n.availableLocales
-    }
-  },
-
-  methods: {
-    activeOn: function(paths) {
-      if(paths.includes(this.$route.name)) {
-        return 'active';
-      } else {
-        return '';
-      }
-    }
-  },
-
-  watch: {
-  }
-}
 </script>
