6087c79fed4fd398474a84e10613975831f6304b
Ivan <ivan.kirnev@gmail.com>
2018-03-09 18:16:56 +0200

    clean

--- app/javascript/packs/components/doors/result.vue
+++ app/javascript/packs/components/doors/result.vue
@@ -5,32 +5,27 @@
       <div class="container">
         <div class="row">
           <nav-left></nav-left>
-
-
-
-<div class="col">
-          <div class="catalog">
-    <div class="container">
-        
-          <div class="catalog__title">Хиты продаж
-            <p>Если у вас возникли сложности с выбором, ознакомьтесь с предложением ниже. Это самые популярные двери, их чаще всего покупают. Список отсортирован по стоимости: от низкой к высокой.</p>
-          </div>
-          <div class="catalog__wrap">
-                <div class="loading" v-if="loading">
-                  Loading...
+          <div class="col">
+            <div class="catalog">
+              <div class="container">
+                <div class="catalog__title">Хиты продаж
+                  <p>Если у вас возникли сложности с выбором, ознакомьтесь с предложением ниже. Это самые популярные двери, их чаще всего покупают. Список отсортирован по стоимости: от низкой к высокой.</p>
                 </div>
-                <template v-for="door in doors">
-                  <a class="catalog__item" href="#">
-                    <div class="image"><img v-bind:src="'/assets/img_storage/' + door.model_path"></div>
-                    <p class="sub-title">{{ door.name }}</p>
-                    <p class="price">{{ door.price }} р.</p><span class="button">купить</span>
-                  </a>
-                </template>
+                <div class="catalog__wrap">
+                  <div class="loading" v-if="loading">
+                    Loading...
+                  </div>
+                  <template v-for="door in doors">
+                    <a class="catalog__item" href="#">
+                      <div class="image"><img v-bind:src="'/assets/img_storage/' + door.model_path"></div>
+                      <p class="sub-title">{{ door.name }}</p>
+                      <p class="price">{{ door.price }} р.</p><span class="button">купить</span>
+                    </a>
+                  </template>
+                </div>
+              </div>
+            </div>
           </div>
-    </div>
-  </div>
-  </div>
-
         </div>
       </div>
     </main>
