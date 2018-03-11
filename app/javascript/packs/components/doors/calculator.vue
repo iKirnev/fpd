616c28a9b7bb5fb5a1715fd805e7ba574bd39a31
Ivan <ivan.kirnev@gmail.com>
2018-03-11 23:04:49 +0200

    part calculator

--- app/javascript/packs/components/doors/calculator.vue
+++ app/javascript/packs/components/doors/calculator.vue
@@ -12,21 +12,21 @@
                   <div class="info-row">
                     <div class="info-row__col"><span class="stage-title">1.<b>ШИРИНА ПРОЕМА</b></span>
                       <span class="calculator-dimm"><input type="text"  v-model="width"></span>
-                      <div class="slider-price"><vue-slider v-model="width" :tooltip="false" :min="650" :max="3000"></vue-slider></div>
+                      <div class="slider-price"><vue-slider v-model="width" v-on:drag-end="dragEnd" :tooltip="false" :min="650" :max="3000"></vue-slider></div>
                     </div>
                     <div class="info-row__col"><span class="stage-title">2.<b>ВЫСОТА ПРОЕМА</b></span>
                       <span class="calculator-dimm"><input type="text" v-model="height"></span>
-                      <div class="slider-price"><vue-slider v-model="height" :tooltip="false" :min="1600" :max="3000"></vue-slider></div>
+                      <div class="slider-price"><vue-slider v-model="height" v-on:drag-end="dragEnd" :tooltip="false" :min="1600" :max="3000"></vue-slider></div>
                     </div>
                   </div>
                   <div class="info-configuration"><span class="stage-title">3.<b>КОНФИГУРАЦИЯ:</b></span>
                     <b-tabs>
-                      <b-tab active><template slot="title"><img src="/assets/conf-image.png"></template></b-tab>
-                      <b-tab><template slot="title"><img src="/assets/conf-image.png"></template></b-tab>
-                      <b-tab><template slot="title"><img src="/assets/conf-image.png"></template></b-tab>
-                      <b-tab><template slot="title"><img src="/assets/conf-image.png"></template></b-tab>
-                      <b-tab><template slot="title"><img src="/assets/conf-image.png"></template></b-tab>
-                      <b-tab><template slot="title"><img src="/assets/conf-image.png"></template></b-tab>
+                      <b-tab @click="selectTab" active><template slot="title"><img src="/assets/conf-image.png"></template></b-tab>
+                      <b-tab @click="selectTab"><template slot="title"><img src="/assets/conf-image.png"></template></b-tab>
+                      <b-tab @click="selectTab"><template slot="title"><img src="/assets/conf-image.png"></template></b-tab>
+                      <b-tab @click="selectTab"><template slot="title"><img src="/assets/conf-image.png"></template></b-tab>
+                      <b-tab @click="selectTab"><template slot="title"><img src="/assets/conf-image.png"></template></b-tab>
+                      <b-tab @click="selectTab"><template slot="title"><img src="/assets/conf-image.png"></template></b-tab>
                     </b-tabs>
                   </div>
                   <div class="info-door">
@@ -41,10 +41,10 @@
                     <div class="info-door__conf"><span class="stage-title">8.<b>ТИП ОТКРЫВАНИЯ:</b></span>
                       <div class="info-door__conf-row">
                         <label class="sb-checkbox sb-checkbox--radio">
-                          <input type="radio"><span></span>Наружу
+                          <input type="radio" v-model="open_type" value="out"><span></span>Наружу
                         </label>
                         <label class="sb-checkbox sb-checkbox--radio">
-                          <input type="radio"><span></span>Во внутрь + 600 р.
+                          <input type="radio" v-model="open_type" value="in" ><span></span>Во внутрь + 600 р.
                         </label>
                       </div><span class="stage-title">9.<b>ЗАМКИ:</b></span>
                       <select>
@@ -52,13 +52,13 @@
                       </select><span class="stage-title">10.<b>УЛУЧШЕНИЯ:</b></span>
                       <div class="info-door__conf-col">
                         <label class="sb-checkbox">
-                          <input type="checkbox"><span></span>Доводчик + 1500 р
+                          <input type="checkbox" v-model="improvements" value="eyehole"><span></span>Глазок
                         </label>
                         <label class="sb-checkbox">
-                          <input type="checkbox"><span></span>Доводчик + 1500 р
+                          <input type="checkbox" v-model="improvements" value="heater_box"><span></span>Утеплитель коробки + 500 р.
                         </label>
                         <label class="sb-checkbox">
-                          <input type="checkbox"><span></span>Доводчик + 1500 р
+                          <input type="checkbox" v-model="improvements" value="closer"><span></span>Доводчик + 1500 р.
                         </label>
                       </div>
                     </div>
@@ -94,10 +94,16 @@ export default {
   components: {
     vueSlider
   },
+  methods: {
+    dragEnd: function (event) {},
+    selectTab: function (event) {}
+  },
   data () {
     return {
       width: 850,
-      height: 2050
+      height: 2050,
+      open_type: 'out',
+      improvements: []
     }
   }
 }
